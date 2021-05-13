82c9edfefeaf261d384d13c27f1e6c45c36ac821
goodmeow <harunbam3@gmail.com>
2021-05-13 16:56:24 +0700

    apollo: initial Nusantara config

--- nad_apollo.mk
+++ nad_apollo.mk
@@ -11,13 +11,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common NusantaraProject stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_apollo
+TARGET_USES_BLUR := true
+USE_PIXEL_CHARGING := true
+NAD_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+USE_GAPPS ?= true
+USE_AOSP_CLOCK := true
+
+# Product
+PRODUCT_NAME := nad_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Xiaomi Mi 10T
+PRODUCT_MODEL ?= Mi 10T Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
