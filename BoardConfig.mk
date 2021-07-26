ejbtrd/device_xiaomi_apollo

apollo: Add libinit for variants handling
Change-Id: Ia990f08d046192a42c0d951a2ce8d06b8794f6f0

BoardConfig.mk
@@ -17,6 +17,10 @@ BUILD_BROKEN_DUP_RULES := true
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_apollo
+TARGET_RECOVERY_DEVICE_MODULES := libinit_apollo
+
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/apollo_defconfig
 

ozankayaa/device_xiaomi_apollo12

Signed-off-by: goodmeow <harunbam3@gmail.com>

BoardConfig.mk
@@ -18,6 +18,10 @@ TARGET_SCREEN_DENSITY := 440
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_apollon
+TARGET_RECOVERY_DEVICE_MODULES := libinit_apollon
+
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/apollo_defconfig
 