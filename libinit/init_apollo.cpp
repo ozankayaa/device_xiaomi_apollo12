3bda573a9c89becfd14b0f315fd46be7a219cc75
goodmeow <harunbam3@gmail.com>
2021-08-27 20:36:40 +0530

    apollo: libinit: remove build description - there is function fingerprint_to_description on common to define this property.

--- libinit/init_apollo.cpp
+++ libinit/init_apollo.cpp
@@ -14,7 +14,6 @@ static const variant_info_t apollocn_info = {
     .device = "apollo",
     .marketname = "Redmi K30S Ultra",
     .model = "M2007J3SC",
-    .build_description = "apollo-user 11 RKQ1.200826.002 V12.5.4.0.RJDCNXM release-keys",
     .build_fingerprint = "Redmi/apollo/apollo:11/RKQ1.200826.002/V12.5.4.0.RJDCNXM:user/release-keys",
 
     .nfc = true,
@@ -28,7 +27,6 @@ static const variant_info_t apollo_info = {
     .device = "apollo",
     .marketname = "Mi 10T",
     .model = "M2007J3SY",
-    .build_description = "apollo-user 11 RKQ1.200826.002 V12.5.2.0.RJDMIXM release-keys",
     .build_fingerprint = "Xiaomi/apollo_global/apollo:11/RKQ1.200826.002/V12.5.2.0.RJDMIXM:user/release-keys",
 
     .nfc = true,
@@ -42,7 +40,6 @@ static const variant_info_t apollopro_info = {
     .device = "apollo",
     .marketname = "Mi 10T Pro",
     .model = "M2007J3SG",
-    .build_description = "apollopro-user 11 RKQ1.200826.002 V12.5.2.0.RJDMIXM release-keys",
     .build_fingerprint = "Xiaomi/apollopro_global/apollo:11/RKQ1.200826.002/V12.5.2.0.RJDMIXM:user/release-keys",
 
     .nfc = true,
